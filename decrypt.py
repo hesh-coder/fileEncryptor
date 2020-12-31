705e88c6f7f18825f1955e1c22a4eeae25e5fe6f
Luca <luca.saleh.2018@live.rhul.ac.uk>
2020-12-31 18:51:42 +0000

    Added imports and function calls inside decrypt.py.

--- decrypt.py
+++ decrypt.py
@@ -1,4 +1,7 @@
+import keyGen
+import sys
 from cryptography.fernet import Fernet
+
 
 def decrypt(filename, key):
     fernClass = Fernet(key)
@@ -6,8 +9,15 @@ def decrypt(filename, key):
     with open(filename, "rb") as file:
         encrypted_data = file.read()
 
-    decrypted_data = f.decrypt(encrypted_data)
+    decrypted_data = fernClass.decrypt(encrypted_data)
 
     new_filename = filename.split(".")[0] + "_decrypted.txt"
     with open(new_filename, "wb") as file:
         file.write(decrypted_data)
+
+key = keyGen.load_key() # now load this key from where we just saved it to
+
+filename = sys.argv[1] # retrieve filename from command line arg
+
+decrypt(filename, key)
+
