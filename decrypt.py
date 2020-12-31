ec8350a94875cf431eeb43f4b644f89e6234fa58
Luca <luca.saleh.2018@live.rhul.ac.uk>
2020-12-31 18:44:13 +0000

    Wrote decrypt function.

--- decrypt.py
+++ decrypt.py
@@ -1,1 +1,13 @@
 from cryptography.fernet import Fernet
+
+def decrypt(filename, key):
+    fernClass = Fernet(key)
+
+    with open(filename, "rb") as file:
+        encrypted_data = file.read()
+
+    decrypted_data = f.decrypt(encrypted_data)
+
+    new_filename = filename.split(".")[0] + "_decrypted.txt"
+    with open(new_filename, "wb") as file:
+        file.write(decrypted_data)
